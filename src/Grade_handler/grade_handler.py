d30f6f692a6b011ed08c4c3e0b456711e9c2827a
i-madifeng <i-madifeng@stepfun.com>
2024-09-22 11:41:26 +0800

    handler start!

--- src/Grade_handler/grade_handler.py
+++ src/Grade_handler/grade_handler.py
@@ -5,21 +5,49 @@ import csv
 
 BASE_PATH = "./data"
 class grade_handler():
-    def __init__(self, std: str = "Grade2gpa.json", grade: str = "Grade.csv") -> None:
+    def __init__(self, std: Grade2gpa, grade: str) -> None:
         self.std = std
-        self.transition = self.grade2gpa(std)
         self._loadgrade(grade)
+        self.terms= set()
+        self.skip = False
+
     
     def _loadgrade(self, grade: str) -> None:
         with open(grade, "r") as f:
             self.grade = list(csv.DictReader(f))
     
     def _grade_filter(self):
+        self.ungraded = []
         for course in self.grade:
             if course["评估状态"] == "未参加评估":
                 print(f"the course {course['课程名称']} in {course['学期']}has not been evaluated")
+                self.ungraded.append(course)
+            self.terms.add(course["学期"])
+        print(f"do you want to skip the unevaluated courses? (y/n)")
+        choice = input() 
+        if choice == "y":
+            self.skip = True
+        for course in self.grade:
+            if course["成绩"] == "合格"|"不合格":
+                self.grade.remove(course)
+                print(f"the course {course['课程名称']} in {course['学期']} has been removed")
+       
     
-    def grade2gpa(self, std: str) -> dict:
-        with open(std, "r") as f:
-            return json.load(f)
+    def mege_ungraded(self):
+        if not self.skip:
+            for course in self.grade:
+                for ug_course in self.ungraded:
+                    if course["课程名称"] == ug_course["课程名称"]:
+                        print(f"please input the grade of the course {course['课程名称']} in {course['学期']}")
+                        while True:
+                            grade = int(input())
+                            if grade <= 100 and grade >= 0 or grade in self.std.transition_dict["Letter Grade System"].keys() or grade in self.std.transition_dict["Five-point Scale"].keys():  
+                                course["成绩"] = grade
+                                break
+                
+        
+
+
+        
+
             
--- src/start.py
+++ src/start.py
@@ -3,10 +3,10 @@ import os
 from Grade_handler.grade2gpa import Grade2gpa
 from Grade_handler.grade_handler import grade_handler
 
-BASE_FILE_PATH = r"./data"
+BASE_FILE_PATH = r"src/data"
 
 def read_data(file_name: str) -> None:
-    file_path = os.path.normpath(os.path.join(BASE_FILE_PATH, file_name))
+    file_path = os.path.join(os.path.join(BASE_FILE_PATH, file_name))
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"文件 {file_path} 不存在")
     df = pd.read_excel(file_path)
@@ -18,7 +18,9 @@ def read_data(file_name: str) -> None:
 
 
 if __name__ == "__main__":
-    std_file  = os.path.normpath(os.path.join(BASE_FILE_PATH, "Grade2gpa.json"))
-    grade_file = os.path.normpath(os.path.join(BASE_FILE_PATH, "Grade.csv"))
+    std_file  = os.path.join(os.path.join(BASE_FILE_PATH, "Grade2gpa.json"))
+    grade_file = os.path.join(os.path.join(BASE_FILE_PATH, "Grade.csv"))
     transition = Grade2gpa(std_file)
+    handler = grade_handler(transition, grade_file)
     transition.show()
+    handler._grade_filter()
